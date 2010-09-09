fbcf50e2076949651797709b38aa65ef12dc9739
morgenro <morgenro@1cffb75b-d306-6187-5d55-5a277715007a>
2010-09-09 17:08:31 +0000

    Fix epidemic routing if bundles are transferred to nodes without epidemic routing extensions.
    git-svn-id: https://www.ibr.cs.tu-bs.de/svn/emma/trunk/src/ibrdtn@29112 1cffb75b-d306-6187-5d55-5a277715007a

--- daemon/src/routing/EpidemicRoutingExtension.cpp
+++ daemon/src/routing/EpidemicRoutingExtension.cpp
@@ -143,15 +143,17 @@ namespace dtn
 					// How to exclude several bundles? Combine two bloomfilters? Use a blocklist. Delay transfers to the node?
 					case dtn::net::TransferAbortedEvent::REASON_REFUSED:
 					{
+						dtn::data::Bundle b = getRouter()->getStorage().get(id);
+
 						// delete the bundle in the storage if
-						if ( EID(eid.getNodeEID()) == EID(meta.destination) )
+						if ( EID(eid.getNodeEID()) == EID(b._destination) )
 						{
 							try {
 								// bundle has been delivered to its destination
 								// TODO: generate a "delete" message for routing algorithm
 
 								// delete it from our storage
-								getRouter()->getStorage().remove(meta);
+								getRouter()->getStorage().remove(b);
 							} catch (dtn::core::BundleStorage::NoBundleFoundException ex) {
 
 							}
