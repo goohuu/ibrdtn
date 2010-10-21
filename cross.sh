36e0295d4505f15f6cc0b89d637145b4d19cda6f
morgenro <morgenro@1cffb75b-d306-6187-5d55-5a277715007a>
2010-10-21 14:47:02 +0000

    Improved cross.sh script to work with a given BUILDROOT toolchain.
    git-svn-id: https://www.ibr.cs.tu-bs.de/svn/emma/trunk/src/ibrdtn@30134 1cffb75b-d306-6187-5d55-5a277715007a

--- cross.sh
+++ cross.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 
+# define $BUILDROOT variable to build against a buildroot toolchain
+
 # get architecture
 if [ -e ".cross-arch" ]; then
 	ARCH=`cat .cross-arch`
@@ -22,15 +24,29 @@ export RANLIB="$ARCH-ranlib"
 export STRIP="$ARCH-strip"
 export OBJCOPY="$ARCH-objcopy"
 export OBJDUMP="$ARCH-objdump"
-export CPPFLAGS="-I/usr/$ARCH/include/c++"
 export CC="$ARCH-gcc"
 export CXX="$ARCH-g++"
+export CPPFLAGS="-I/usr/$ARCH/include/c++"
+
+if [ -n "$BUILDROOT" ]; then
+	export CPPFLAGS="-I$BUILDROOT/output/staging/usr/include \
+		-I$BUILDROOT/output/staging/usr/include/c++ \
+		-I$BUILDROOT/output/toolchain/uClibc_dev/usr/include \
+		-I$BUILDROOT/output/toolchain/linux/include"
+
+	export LDFLAGS="-L$BUILDROOT/output/staging/usr/lib \
+		-L$BUILDROOT/output/toolchain/uClibc_dev/usr/lib"
+
+	export PATH="$PATH:$BUILDROOT/output/staging/usr/bin"
+fi
 
 if [ "$1" == "compile" ]; then
 	make
 elif [ "$1" == "configure" ]; then
 	export ac_cv_func_malloc_0_nonnull=yes
-	./configure --target=$ARCH --host=$ARCH --build=$HOST
+	./configure CXXFLAGS="-g3 -ggdb -Wall -Wextra" --target=$ARCH --host=$ARCH --build=$HOST --prefix=/opt/ibrdtn-$ARCH
+elif [ "$1" == "install" ]; then
+	make install
 else
 	echo "$1" > .cross-arch
 	echo "$2" > .cross-host
