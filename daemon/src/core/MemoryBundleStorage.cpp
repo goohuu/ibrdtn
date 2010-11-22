c29fd7e4a2bad1bc713074faea71d79118bfc269
morgenro <morgenro@1cffb75b-d306-6187-5d55-5a277715007a>
2010-11-22 10:47:35 +0000

    First version of the memory based storage fork.
    git-svn-id: https://www.ibr.cs.tu-bs.de/svn/emma/trunk/src/ibrdtn@30970 1cffb75b-d306-6187-5d55-5a277715007a

---
+++ daemon/src/core/MemoryBundleStorage.cpp
@@ -0,0 +1,293 @@
+/*
+ * MemoryBundleStorage.cpp
+ *
+ *  Created on: 22.11.2010
+ *      Author: morgenro
+ */
+
+#include "core/MemoryBundleStorage.h"
+#include "core/TimeEvent.h"
+#include "core/GlobalEvent.h"
+#include "core/BundleExpiredEvent.h"
+#include "core/BundleEvent.h"
+
+#include <ibrcommon/Logger.h>
+#include <ibrcommon/thread/MutexLock.h>
+
+namespace dtn
+{
+	namespace core
+	{
+		MemoryBundleStorage::MemoryBundleStorage(size_t maxsize)
+		 : _maxsize(maxsize), _currentsize(0)
+		{
+		}
+
+		MemoryBundleStorage::~MemoryBundleStorage()
+		{
+		}
+
+		void MemoryBundleStorage::componentUp()
+		{
+			bindEvent(TimeEvent::className);
+		}
+
+		void MemoryBundleStorage::componentDown()
+		{
+			unbindEvent(TimeEvent::className);
+		}
+
+		void MemoryBundleStorage::raiseEvent(const Event *evt)
+		{
+			try {
+				const TimeEvent &time = dynamic_cast<const TimeEvent&>(*evt);
+				if (time.getAction() == dtn::core::TIME_SECOND_TICK)
+				{
+					// do expiration of bundles
+					ibrcommon::MutexLock l(_bundleslock);
+					dtn::data::BundleList::expire(time.getTimestamp());
+				}
+			} catch (const std::bad_cast&) { }
+		}
+
+		const std::string MemoryBundleStorage::getName() const
+		{
+			return "MemoryBundleStorage";
+		}
+
+		bool MemoryBundleStorage::empty()
+		{
+			ibrcommon::MutexLock l(_bundleslock);
+			return _bundles.empty();
+		}
+
+		void MemoryBundleStorage::releaseCustody(dtn::data::BundleID&)
+		{
+			// custody is successful transferred to another node.
+			// it is safe to delete this bundle now. (depending on the routing algorithm.)
+		}
+
+		unsigned int MemoryBundleStorage::count()
+		{
+			ibrcommon::MutexLock l(_bundleslock);
+			return _bundles.size();
+		}
+
+		const dtn::data::MetaBundle MemoryBundleStorage::getByFilter(const ibrcommon::BloomFilter &filter)
+		{
+			// search for one bundle which is not contained in the filter
+			// until we have a better strategy, we have to iterate through all bundles
+			ibrcommon::MutexLock l(_bundleslock);
+			for (std::set<dtn::data::Bundle>::const_iterator iter = _bundles.begin(); iter != _bundles.end(); iter++)
+			{
+				const dtn::data::Bundle &bundle = (*iter);
+
+				try {
+					if ( !filter.contains(bundle.toString()) )
+					{
+						// check if the destination is blocked by the destination filter
+						if ( (_destination_filter.find(bundle._destination) == _destination_filter.end()) )
+						{
+							IBRCOMMON_LOGGER_DEBUG(10) << bundle.toString() << " is not in the bloomfilter" << IBRCOMMON_LOGGER_ENDL;
+							return bundle;
+						}
+					}
+				} catch (const dtn::InvalidDataException &ex) {
+					IBRCOMMON_LOGGER_DEBUG(10) << "InvalidDataException on bundle get: " << ex.what() << IBRCOMMON_LOGGER_ENDL;
+				}
+			}
+
+			throw BundleStorage::NoBundleFoundException();
+		}
+
+		const dtn::data::MetaBundle MemoryBundleStorage::getByDestination(const dtn::data::EID &eid, bool exact)
+		{
+			IBRCOMMON_LOGGER_DEBUG(5) << "Storage: get bundle for " << eid.getString() << IBRCOMMON_LOGGER_ENDL;
+
+			ibrcommon::MutexLock l(_bundleslock);
+			for (std::set<dtn::data::Bundle>::const_iterator iter = _bundles.begin(); iter != _bundles.end(); iter++)
+			{
+				const dtn::data::Bundle &bundle = (*iter);
+
+				try {
+					if (exact)
+					{
+						if (bundle._destination == eid)
+						{
+							return bundle;
+						}
+					}
+					else
+					{
+						if (bundle._destination.getNodeEID() == eid.getNodeEID())
+						{
+							return bundle;
+						}
+					}
+				} catch (const dtn::InvalidDataException &ex) {
+					IBRCOMMON_LOGGER_DEBUG(10) << "InvalidDataException on bundle get: " << ex.what() << IBRCOMMON_LOGGER_ENDL;
+				}
+			}
+
+			throw BundleStorage::NoBundleFoundException();
+		}
+
+		dtn::data::Bundle MemoryBundleStorage::get(const dtn::data::BundleID &id)
+		{
+			try {
+				ibrcommon::MutexLock l(_bundleslock);
+
+				for (std::set<dtn::data::Bundle>::const_iterator iter = _bundles.begin(); iter != _bundles.end(); iter++)
+				{
+					const dtn::data::Bundle &bundle = (*iter);
+					if (id == bundle)
+					{
+						return bundle;
+					}
+				}
+			} catch (const dtn::SerializationFailedException &ex) {
+				// bundle loading failed
+				IBRCOMMON_LOGGER(error) << "Error while loading bundle data: " << ex.what() << IBRCOMMON_LOGGER_ENDL;
+
+				// the bundle is broken, delete it
+				remove(id);
+
+				throw BundleStorage::BundleLoadException();
+			}
+
+			throw BundleStorage::NoBundleFoundException();
+		}
+
+		void MemoryBundleStorage::store(const dtn::data::Bundle &bundle)
+		{
+			ibrcommon::MutexLock l(_bundleslock);
+
+			// get size of the bundle
+			dtn::data::DefaultSerializer s(std::cout);
+			size_t size = s.getLength(bundle);
+
+			// check if this container is too big for us.
+			if ((_maxsize > 0) && (_currentsize + size > _maxsize))
+			{
+				throw StorageSizeExeededException();
+			}
+
+			// increment the storage size
+			_currentsize += size;
+
+			// insert Container
+			pair<set<dtn::data::Bundle>::iterator,bool> ret = _bundles.insert( bundle );
+
+			if (ret.second)
+			{
+				dtn::data::BundleList::add(dtn::data::MetaBundle(bundle));
+			}
+			else
+			{
+				IBRCOMMON_LOGGER_DEBUG(5) << "Storage: got bundle duplicate " << bundle.toString() << IBRCOMMON_LOGGER_ENDL;
+			}
+		}
+
+		void MemoryBundleStorage::remove(const dtn::data::BundleID &id)
+		{
+			ibrcommon::MutexLock l(_bundleslock);
+
+			for (std::set<dtn::data::Bundle>::const_iterator iter = _bundles.begin(); iter != _bundles.end(); iter++)
+			{
+				if ( id == (*iter) )
+				{
+					// remove item in the bundlelist
+					dtn::data::Bundle bundle = (*iter);
+
+					// remove it from the bundle list
+					dtn::data::BundleList::remove(bundle);
+
+					// get size of the bundle
+					dtn::data::DefaultSerializer s(std::cout);
+					size_t size = s.getLength(bundle);
+
+					// decrement the storage size
+					_currentsize -= size;
+
+					// remove the container
+					_bundles.erase(iter);
+
+					return;
+				}
+			}
+
+			throw BundleStorage::NoBundleFoundException();
+		}
+
+		dtn::data::MetaBundle MemoryBundleStorage::remove(const ibrcommon::BloomFilter &filter)
+		{
+			ibrcommon::MutexLock l(_bundleslock);
+
+			for (std::set<dtn::data::Bundle>::const_iterator iter = _bundles.begin(); iter != _bundles.end(); iter++)
+			{
+				const dtn::data::Bundle &bundle = (*iter);
+
+				if ( filter.contains(bundle.toString()) )
+				{
+					// remove it from the bundle list
+					dtn::data::BundleList::remove(bundle);
+
+					// get size of the bundle
+					dtn::data::DefaultSerializer s(std::cout);
+
+					// decrement the storage size
+					_currentsize -= s.getLength(bundle);
+
+					// remove the container
+					_bundles.erase(iter);
+
+					return (MetaBundle)bundle;
+				}
+			}
+
+			throw BundleStorage::NoBundleFoundException();
+		}
+
+		size_t MemoryBundleStorage::size() const
+		{
+			return _currentsize;
+		}
+
+		void MemoryBundleStorage::clear()
+		{
+			ibrcommon::MutexLock l(_bundleslock);
+
+			_bundles.clear();
+			dtn::data::BundleList::clear();
+
+			// set the storage size to zero
+			_currentsize = 0;
+		}
+
+		void MemoryBundleStorage::eventBundleExpired(const ExpiringBundle &b)
+		{
+			for (std::set<dtn::data::Bundle>::const_iterator iter = _bundles.begin(); iter != _bundles.end(); iter++)
+			{
+				if ( b.bundle == (*iter) )
+				{
+					dtn::data::Bundle bundle = (*iter);
+
+					// get size of the bundle
+					dtn::data::DefaultSerializer s(std::cout);
+
+					// decrement the storage size
+					_currentsize -= s.getLength(bundle);
+
+					_bundles.erase(iter);
+					break;
+				}
+			}
+
+			// raise bundle event
+			dtn::core::BundleEvent::raise( b.bundle, dtn::core::BUNDLE_DELETED, dtn::data::StatusReportBlock::LIFETIME_EXPIRED);
+
+			// raise an event
+			dtn::core::BundleExpiredEvent::raise( b.bundle );
+		}
+	}
+}
---
+++ daemon/src/core/MemoryBundleStorage.h
@@ -0,0 +1,126 @@
+/*
+ * MemoryBundleStorage.h
+ *
+ *  Created on: 22.11.2010
+ *      Author: morgenro
+ */
+
+#ifndef MEMORYBUNDLESTORAGE_H_
+#define MEMORYBUNDLESTORAGE_H_
+
+#include "Component.h"
+#include "core/BundleCore.h"
+#include "core/BundleStorage.h"
+#include "core/Node.h"
+#include "core/EventReceiver.h"
+
+#include <ibrdtn/data/Bundle.h>
+#include <ibrdtn/data/BundleList.h>
+
+namespace dtn
+{
+	namespace core
+	{
+		class MemoryBundleStorage : public BundleStorage, public EventReceiver, public dtn::daemon::IntegratedComponent, private dtn::data::BundleList
+		{
+		public:
+			MemoryBundleStorage(size_t maxsize = 0);
+			virtual ~MemoryBundleStorage();
+
+			/**
+			 * Stores a bundle in the storage.
+			 * @param bundle The bundle to store.
+			 */
+			virtual void store(const dtn::data::Bundle &bundle);
+
+			/**
+			 * This method returns a specific bundle which is identified by
+			 * its id.
+			 * @param id The ID of the bundle to return.
+			 * @return A bundle object of the
+			 */
+			virtual dtn::data::Bundle get(const dtn::data::BundleID &id);
+
+			/**
+			 * Query for a bundle with a specific destination. Set exact to true, if the application
+			 * part should be compared too.
+			 * @param eid
+			 * @param exact
+			 * @return
+			 */
+			virtual const dtn::data::MetaBundle getByDestination(const dtn::data::EID &eid, bool exact = false);
+
+			/**
+			 * Returns a bundle ID which is not in the bloomfilter, but in the storage
+			 * @param filter
+			 * @return
+			 */
+			virtual const dtn::data::MetaBundle getByFilter(const ibrcommon::BloomFilter &filter);
+
+			/**
+			 * This method deletes a specific bundle in the storage.
+			 * No reports will be generated here.
+			 * @param id The ID of the bundle to remove.
+			 */
+			void remove(const dtn::data::BundleID &id);
+
+			/**
+			 * Remove one bundles which match this filter
+			 * @param filter
+			 * @return The bundle meta data of the removed bundle.
+			 */
+			dtn::data::MetaBundle remove(const ibrcommon::BloomFilter &filter);
+
+			/**
+			 * @sa BundleStorage::clear()
+			 */
+			void clear();
+
+			/**
+			 * @sa BundleStorage::empty()
+			 */
+			bool empty();
+
+			/**
+			 * @sa BundleStorage::count()
+			 */
+			unsigned int count();
+
+			/**
+			 * Get the current size
+			 */
+			size_t size() const;
+
+			/**
+			 * @sa BundleStorage::releaseCustody();
+			 */
+			void releaseCustody(dtn::data::BundleID &bundle);
+
+			/**
+			 * This method is used to receive events.
+			 * @param evt
+			 */
+			void raiseEvent(const Event *evt);
+
+			/**
+			 * @see Component::getName()
+			 */
+			virtual const std::string getName() const;
+
+		protected:
+			virtual void componentUp();
+			virtual void componentDown();
+
+			virtual void eventBundleExpired(const ExpiringBundle &b);
+
+		private:
+			ibrcommon::Mutex _bundleslock;
+			std::set<dtn::data::Bundle> _bundles;
+
+			size_t _maxsize;
+			size_t _currentsize;
+		};
+	}
+}
+
+#endif /* MEMORYBUNDLESTORAGE_H_ */
