a04024be411d8c1669bc1dadec2a768ea793f432
morgenro <morgenro@1cffb75b-d306-6187-5d55-5a277715007a>
2011-03-21 15:19:07 +0000

    Less includes.
    git-svn-id: https://www.ibr.cs.tu-bs.de/svn/emma/trunk/src/ibrdtn@34397 1cffb75b-d306-6187-5d55-5a277715007a

--- daemon/src/Main.cpp
+++ daemon/src/Main.cpp
@@ -58,11 +58,8 @@
 #endif
 
 #ifdef HAVE_LIBDAEMON
-#include <libdaemon/dfork.h>
-#include <libdaemon/dsignal.h>
-#include <libdaemon/dlog.h>
-#include <libdaemon/dpid.h>
-#include <libdaemon/dexec.h>
+#include <libdaemon/daemon.h>
+#include <string.h>
 #endif
 
 #include <csignal>
