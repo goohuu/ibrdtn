d6a1967d5ec6050ae40c0fa6b097e78779ea45d4
morgenro <morgenro@1cffb75b-d306-6187-5d55-5a277715007a>
2011-04-19 15:04:09 +0000

    HTTPConvergenceLayer by Robert Heitz
    git-svn-id: https://www.ibr.cs.tu-bs.de/svn/emma/trunk/src/ibrdtn@35709 1cffb75b-d306-6187-5d55-5a277715007a

--- daemon/src/net/HTTPConvergenceLayer.h
+++ daemon/src/net/HTTPConvergenceLayer.h
@@ -1,19 +1,46 @@
-/*
- * HTTPConvergenceLayer.h
+/**
+ * @file HTTPConvergenceLayer.h
+ * @date: 08.03.2011
+ * @author : Robert Heitz
  *
- *  Created on: 29.07.2010
- *      Author: morgenro
+ * HTTPConvergenceLayer header file. In this file the classes
+ * DownloadThread and HTTPConvergenceLayer are defined. The class
+ * HTTPConvergenceLayer implements the interfaces ConvergenceLayer
+ * and IndependentComponent. The class DownloadThread implemets the
+ * JoinableThread interface.
  */
+
 
 #ifndef HTTPCONVERGENCELAYER_H_
 #define HTTPCONVERGENCELAYER_H_
 
 #include "Component.h"
+
+#include "core/BundleCore.h"
+#include "core/BundleEvent.h"
+
+#include <ibrdtn/data/Serializer.h>
+
 #include "net/ConvergenceLayer.h"
-#include <ibrcommon/Exceptions.h>
+#include "net/TransferCompletedEvent.h"
+#include "net/TransferAbortedEvent.h"
+#include "net/BundleReceivedEvent.h"
+
+#include <ibrcommon/data/BLOB.h>
+#include <ibrcommon/data/iobuffer.h>
+
 #include <ibrcommon/net/vinterface.h>
 #include <ibrcommon/net/vaddress.h>
+#include <ibrcommon/thread/MutexLock.h>
 #include <ibrcommon/thread/Mutex.h>
+#include <ibrcommon/thread/Thread.h>
+#include <ibrcommon/Exceptions.h>
+#include <ibrcommon/Logger.h>
+
+#include <curl/curl.h>
+#include <curl/easy.h>
+
+
 #include <iostream>
 
 namespace dtn
@@ -27,12 +54,17 @@ namespace dtn
 			virtual ~HTTPConvergenceLayer();
 
 			dtn::core::Node::Protocol getDiscoveryProtocol() const;
+
+			/**
+			 * @see ConvergenceLayer::queue()
+			 */
 			void queue(const dtn::core::Node &n, const ConvergenceLayer::Job &job);
 
 			/**
 			 * @see Component::getName()
 			 */
 			virtual const std::string getName() const;
+
 
 		protected:
 			virtual void componentUp();
@@ -41,9 +73,34 @@ namespace dtn
 			bool __cancellation();
 
 		private:
-			ibrcommon::Mutex _write_lock;
+			/** Variable contains Tomcat-Server URL, which is specified
+			 * in the IBR-DTN configuration file.
+			 */
 			const std::string _server;
+
+			/** For managing access for iobuffer */
+			ibrcommon::Mutex _push_iob_mutex;
+			/** IO buffer in witch the received data will be witten.*/
+			ibrcommon::iobuffer *_push_iob;
+			/** variable to control the independent thread, when true the thread is running */
+			bool _running;
 		};
+
+		class DownloadThread : public ibrcommon::JoinableThread
+		{
+		public:
+
+			DownloadThread(ibrcommon::iobuffer &buf);
+			virtual ~DownloadThread();
+
+		protected:
+			void run();
+
+		private:
+			/** istream variable is using for reading from iobuffer */
+			std::istream _stream;
+		};
+
 	}
 }
 
