0182fcbe8b2aa0976a2d19cf215902927d5dd430
Stefan Schmidt <stefan@datenfreihafen.org>
2011-08-01 15:43:51 +0200

    lowpan: 114 is fine again now

--- daemon/src/net/LOWPANConvergenceLayer.cpp
+++ daemon/src/net/LOWPANConvergenceLayer.cpp
@@ -138,20 +138,20 @@ namespace dtn
 
 				cout << "Send out bundle with length " << dec << data.length() << endl;
 
-				if (data.length() > 113) {
+				if (data.length() > 114) {
 					std::string chunk, tmp;
 					char header;
 					stringstream buf;
 					int i;
-					int chunks = ceil(data.length() / 113.0);
+					int chunks = ceil(data.length() / 114.0);
 					cout << "Bundle to big to fit into one packet. Need to split into " << dec << chunks << " segments" << endl;
-					for (i = 0; i < data.length(); i += 113) {
-						chunk = data.substr(i, 113);
+					for (i = 0; i < data.length(); i += 114) {
+						chunk = data.substr(i, 114);
 
 						header = 0;
 						if (i == 0) // First segment
 							header = SEGMENT_FIRST;
-						if (data.length() < i + 113) // Last segment
+						if (data.length() < i + 114) // Last segment
 							header = SEGMENT_LAST;
 
 						buf << header;
