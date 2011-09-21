4cb5da59584e6cc57026d37bd91b54c284759998
morgenro <morgenro@1cffb75b-d306-6187-5d55-5a277715007a>
2011-09-21 16:15:38 +0000

    Script for auto-using USB drives with file convergence layer.
    git-svn-id: https://www.ibr.cs.tu-bs.de/svn/emma/trunk/src/ibrdtn@43792 1cffb75b-d306-6187-5d55-5a277715007a

---
+++ contrib/dtndrive.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+#
+
+MOUNTDIR=${1}
+
+while [ 1 == 1 ]; do
+	inotifywait -e create -e delete /media
+	FILES=`find ${MOUNTDIR} -maxdepth 2 -type f -name '.dtndrive'`
+	for DESC in $FILES; do
+		DTNPATH=`dirname ${DESC}`
+		echo "dtn drive directory found: ${DTNPATH}"
+
+		# gen lock file
+		LOCK="/tmp/`basename ${DTNPATH}.dtndrive`"
+
+		# check if the path is already mounted
+		if [ ! -e ${LOCK} ]; then
+			cp ${DESC} ${LOCK}
+			echo "DTNPATH=\"${DTNPATH}\"" >> ${LOCK}
+
+			# load configuration
+			. ${DESC}
+
+			# announce node to the dtnd
+			echo "connection ${EID} file add file://${DTNPATH}/${STORAGE}" | netcat localhost 4550 > /dev/null
+		fi
+	done
+
+	# check if some paths are gone
+	LOCKS=`find /tmp -maxdepth 1 -type f -name '*.dtndrive'`
+	for L in ${LOCKS}; do
+		echo "check lock file: ${L}"
+		. ${L}
+		if [ ! -d ${DTNPATH} ]; then
+			echo "medium removed"
+			rm ${L}
+
+			# announce node to the dtnd
+			echo "connection ${EID} file del file://${DTNPATH}/${STORAGE}" | netcat localhost 4550  > /dev/null
+		fi
+	done
+done
+
